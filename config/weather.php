046db7970a17f965b9ce1a6dc2a320b3dacdcf81
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-03-25 12:43:06 +0530

    feat: Add weather.php config file

---
+++ config/weather.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'divisions' => [
+        [
+            'name' => 'Chittagong',
+            'districts' => ['Comilla', 'Feni', 'Brahmanbaria', 'Rangamati', 'Noakhali', 'Chandpur', 'Lakshmipur', 'Chittagong', 'Coxs Bazar', 'Khagrachhari', 'Bandarban']
+        ],
+        [
+            'name' => 'Rajshahi',
+            'districts' => ['Sirajganj', 'Pabna', 'Bogra', 'Rajshahi', 'Natore', 'Joypurhat', 'Nawabganj', 'Naogaon']
+        ],
+        [
+            'name' => 'Khulna',
+            'districts' => ['Jessore', 'Satkhira', 'Meherpur', 'Narail', 'Chuadanga', 'Kushtia', 'Magura', 'Khulna', 'Bagerhat', 'Jhenaidah']
+        ],
+        [
+            'name' => 'Barisal',
+            'districts' => ['Jhalokati', 'Patuakhali', 'Pirojpur', 'Barisal', 'Bhola', 'Barguna']
+        ],
+        [
+            'name' => 'Sylhet',
+            'districts' => ['Sylhet', 'Moulvibazar', 'Habiganj', 'Sunamganj']
+        ],
+        [
+            'name' => 'Dhaka',
+            'districts' => ['Narsingdi', 'Gazipur', 'Shariatpur', 'Narayanganj', 'Tangail', 'Kishoreganj', 'Manikganj', 'Dhaka', 'Munshiganj', 'Rajbari', 'Madaripur', 'Gopalganj', 'Faridpur']
+        ],
+        [
+            'name' => 'Rangpur',
+            'districts' => ['Panchagarh', 'Dinajpur', 'Lalmonirhat', 'Nilphamari', 'Gaibandha', 'Thakurgaon', 'Rangpur', 'Kurigram']
+        ],
+        [
+            'name' => 'Mymensingh',
+            'districts' => ['Sherpur', 'Mymensingh', 'Jamalpur', 'Netrokona']
+        ]
+    ]
+];
